7be8cbc5c466103d58818f67174be6e8c5a14dea
Sven Rademakers <svenrademakers@icloud.com>
2022-05-13 12:53:43 +0200

    tryout aarch64-unknown-none

--- scripts/build_aarch64_openwrt.sh
+++ scripts/build_aarch64_openwrt.sh
@@ -6,10 +6,10 @@ function install_rust() {
   chmod +x /tmp/rustup.sh  
   /tmp/rustup.sh -y --profile minimal --target $RUST_TARGET
   source $HOME/.cargo/env
-  cargo install cargo-get
 }
 
 #. /root/env/bcm-hnd-802.11ax.sh
+ln -sf /root/am-toolchains/brcm-arm-hnd /opt/toolchains
 export LD_LIBRARY_PATH=/opt/toolchains/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/lib:$LD_LIBRARY
 export TOOLCHAIN_BASE=/opt/toolchains
 export PATH=/opt/toolchains/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/bin:$PATH
