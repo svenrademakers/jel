e7e25c3b350ffed8e8222ad731f9915f368c425f
Sven Rademakers <sven.rademakers@gmail.com>
2022-05-13 08:36:18 +0200

    Update config

--- scripts/build_aarch64_openwrt.sh
+++ scripts/build_aarch64_openwrt.sh
@@ -9,7 +9,12 @@ function install_rust() {
   cargo install cargo-get
 }
 
-. /root/env/bcm-hnd-802.11ax.sh
+#. /root/env/bcm-hnd-802.11ax.sh
+export LD_LIBRARY_PATH=/opt/toolchains/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/lib:$LD_LIBRARY
+export TOOLCHAIN_BASE=/opt/toolchains
+export PATH=/opt/toolchains/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/bin:$PATH
+export PATH=/opt/toolchains/crosstools-aarch64-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/bin:$PATH
+
 install_rust
 aarch64-linux-gcc -v
 TARGET_CC=aarch64-linux-gcc cargo build --manifest-path http_server/Cargo.toml --release --target $RUST_TARGET -vv
