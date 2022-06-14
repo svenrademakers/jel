7da6525dc7cd9b18b8567f52ca8e290f7efddb6b
Sven Rademakers <sven.rademakers@silabs.com>
2022-06-14 07:24:19 +0200

    created skeleton for closures as services

--- hyper_rusttls/src/service.rs
+++ hyper_rusttls/src/service.rs
@@ -10,3 +10,44 @@ pub trait RequestHandler: Send + Sync + Display {
     where
         Self: Sized;
 }
+
+pub type BoxedServiceHandler =
+    Box<dyn Fn(http::Request<Body>) -> std::io::Result<http::Response<Body>>>;
+
+#[macro_export]
+macro_rules! service_function {
+    ($name: ident, $func: expr, $uri: literal) => {
+        #[derive(Debug)]
+        pub struct ServiceFunction$ident {
+            closure: BoxedServiceHandler,
+        }
+
+        impl ServiceFunction$ident {
+            pub fn new(closure: BoxedServiceHandler) -> Self {
+                Self { closure }
+            }
+        }
+
+        #[async_trait]
+        impl RequestHandler for ServiceFunction$ident {
+            async fn invoke(
+                &self,
+                request: http::Request<Body>,
+            ) -> std::io::Result<http::Response<Body>> {
+                self.closure(request)
+            }
+
+            fn path() -> &'static str {
+                $uri
+            }
+        }
+
+        impl Display for ServiceFunction$ident {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", stringify!($name))
+            }
+        }
+
+        ServiceFunction$ident::new($func)
+    };
+}
